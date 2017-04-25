4bcefe12a8b57d6e969aa076eba06c4cfa2d4433
Karthikeyan1993 <karthik.infotec99@gmail.com>
2017-04-25 10:11:10 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+var count;
+count=0;
+//for loop iteration
+for (var i = 1; i <= 1000; i++) {
+  count=count+i;
+}
+console.log(count);
+
+var i=1;
+count=0;
+//while loop iteration
+while (i<=1000)
+{
+  count=count+i;
+  i++;
+}
+
+console.log(count);
